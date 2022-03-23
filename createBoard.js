266714660a751141dfb37be9ec7a38eb0d1b3f59
Joshua Simon <joshy.d.simon@gmail.com>
2022-03-24 10:55:56 +1300

    refactoring

--- createBoard.js
+++ createBoard.js
@@ -15,11 +15,15 @@ const initialBoardState = [
 //denoted by 'O'
 let xRandomValue;
 let yRandomValue;
+let xRandomValue1;
+let yRandomValue1;
 let coordinates = [];
 
 const generateBoard = (board) => {
-   xRandomValue = Math.floor(Math.random() * 8);
-   yRandomValue = Math.floor(Math.random() * 8);
+  xRandomValue = Math.floor(Math.random() * 8);
+  yRandomValue = Math.floor(Math.random() * 8);
+  xRandomValue1 = Math.floor(Math.random() * 8);
+  yRandomValue1 = Math.floor(Math.random() * 8);
 
   for (let i = 0; i < board.length; i++) {
     if (i === yRandomValue) {
@@ -30,15 +34,20 @@ const generateBoard = (board) => {
       }
     }
   }
-  coordinates.push(xRandomValue)
-  coordinates.push(yRandomValue)
+
+  for (let i = 0; i < board.length; i++) {
+    if (i === yRandomValue) {
+      for (let j = 0; j < board[yRandomValue1].length; j++) {
+        if (j === xRandomValue1) {
+          board[yRandomValue1][xRandomValue1] = ["O"];
+        }
+      }
+    }
+  }
+  coordinates.push(xRandomValue, yRandomValue, xRandomValue1, yRandomValue1);
+
   return board;
 };
 
-//Running the 'generateBoard' once on a blank board generates
-//outputs a board with a single ship. If you pass that output
-//through the 'generateBoard' function again, it will output
-//a board with the required two ships. 
-const newBoardWithOneShip = generateBoard(initialBoardState)
-export const newBoardWithTwoShips = generateBoard(newBoardWithOneShip)
+export const newBoardWithTwoShips = generateBoard(initialBoardState)
 export {coordinates}

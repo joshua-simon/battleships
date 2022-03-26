bc94bdcae40a7b353e14879cb105e357ad4f628c
Joshua Simon <joshy.d.simon@gmail.com>
2022-03-26 14:27:43 +1300

    more refactoring

--- createBoard.js
+++ createBoard.js
@@ -12,22 +12,19 @@ const initialBoardState = [
 ]
 
 //generateBoard: A function that generates a game board with 1 single cell "ship",
-//denoted by 'O'
-let xRandomValue;
-let yRandomValue;
-let xRandomValue1;
-let yRandomValue1;
+
 let coordinates = [];
 
 const generateBoard = (board) => {   
   for (var i=0; i<2;i++) { 
     const x = Math.floor(Math.random() * 8);     
     const y = Math.floor(Math.random() * 8);     
-    board[y][x] = ['0']; 
+    board[y][x] = ['🚢']; 
     coordinates.push(x,y)
    }   
    return board; 
   }
+
 
 export const newBoardWithTwoShips = generateBoard(initialBoardState)
 export {coordinates}
